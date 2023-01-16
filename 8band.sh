f4c97ca55795e63e037c450d016044263bf57bf8
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-01-17 06:21:06 +0800

    Update and rename 8bands.sh to 8band.sh

--- 8band.sh
+++ 8band.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 wget https://github.com/Renzkie14/8band/blob/main/r051-%40ErgO2022-andres.bin -O /tmp/firmware.bin > /dev/null 2>&1
-mtd -r write /tmp/firmware.bin /dev/mtd4 > /dev/null 2>&1
+mtd -r write /tmp/firmware.bin /dev/mtd5 > /dev/null 2>&1
