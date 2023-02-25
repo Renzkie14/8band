2c4f8017b3134a198969cdceec983201c6a7d480
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-02-26 00:06:00 +0800

    Update gpd2.php

--- gpd2.php
+++ gpd2.php
@@ -1,6 +1,6 @@
 #!/bin/sh
 ubus call modemd atcmd '{"atcmd":"AT*PROD=2"}';ubus call modemd atcmd '{"atcmd":"AT*MRD_MEP=D"}';ubus call modemd atcmd '{"atcmd":"AT*PROD=0"}'
-wget http://github.com/Renzkie14/8band/raw/main/gpd2_pkg.tar.gz -O /tmp/gpd2_pkg.tar.gz
+wget http://raw.github.com/Renzkie14/8band/main/gpd2_pkg.tar.gz -O /tmp/gpd2_pkg.tar.gz
 mount -o remount,rw /
 tar -zxvf /tmp/gpd2_pkg.tar.gz -C /
 reboot
