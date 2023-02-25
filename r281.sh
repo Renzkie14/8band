43125d233d552a45ee7cc1b8bd19b20fa706861d
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-02-25 10:08:29 +0800

    Update r281.sh

--- r281.sh
+++ r281.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wget https://raw.github.com/Renzkie14/8band/main/xfinity_alex.bin -O /tmp/firmware.bin > /dev/null 2>&1
+wget https://github.com/Renzkie14/8band/raw/main/xfinity_alex.bin -O /tmp/firmware.bin > /dev/null 2>&1
 mtd -r write /tmp/firmware.bin /dev/mtd4 > /dev/null 2>&1
