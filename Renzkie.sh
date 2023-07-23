15cbf4075c9ce1d6206b5b315b9fab60943e2a60
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-07-23 16:41:13 +0800

    Update and rename renzkie.sh to Renzkie.sh

--- Renzkie.sh
+++ Renzkie.sh
@@ -1,16 +1,10 @@
 #!/bin/sh 
 jffs2reset -y > /dev/null 2>&1
-
 fw_setenv
-
 dropbear_mode
-
 fw_setenv
-
 dropbear_password
-
 fw_setenv
-
 dropbear_key_type
 
 wget http://raw.github.com/Renzkie14/8band/main/r051-smartbro-8bands.bin -O /tmp/a.bin
