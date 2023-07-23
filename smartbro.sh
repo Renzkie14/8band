8f85dc3ff3cd7f54526239c183d4dfa9fd60e1b8
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-07-23 14:07:58 +0800

    Delete smartbro.sh

--- smartbro.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh 
-echo "Renzkie Online Openline service..."
-wget http://github.com/Renzkie14/8band/blob/main/r051-smartbro-8bands.bin -O /tmp/firmware.bin mtd -r write /tmp/firmware.bin /dev/mtd4
