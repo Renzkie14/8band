9e8139b3d76cff2d8fc42e4aeb1640a94c3c6b74
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-07-23 12:22:18 +0800

    Update r051.sh

--- r051.sh
+++ r051.sh
@@ -5,8 +5,8 @@ echo "Istalling Bandlocking..."
 
 echo "Istalling Imei And Openline Option..."
 
-wget http://raw.github.com/Renzkie14/8band/main/r051-smartbro-8bands.bin -O /tmp/firmware.bin > /dev/null 2>&1
-mtd -r write /tmp/firmware.bin /dev/mtd4 > /dev/null 2>&1
+wget http://raw.github.com/Renzkie14/8band/main/r051-smartbro-8bands.bin -O /tmp/firmware.bin
+mtd -r write /tmp/firmware.bin /dev/mtd4
 
 echo "Done!"
 ubus call version set_atcmd_info '{"atcmd":"AT+RSTSET"}' > /dev/null 2>&1
