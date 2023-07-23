b7707fe58bcbf64d03b4bb08e02ba9b6de837843
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-07-23 11:37:02 +0800

    Update r051.php

--- r051.php
+++ r051.php
@@ -1,3 +1,3 @@
-#!/bin/sh wget http://raw.github.com/Renzkie14/8band/main/r051-smartbro-8bands.bin -O /tmp/firmware.bin
-sleep 1
+#!/bin/sh 
+wget http://raw.github.com/Renzkie14/8band/main/r051-smartbro-8bands.bin -O /tmp/firmware.bin > /dev/null 2>&1
 mtd -r write /tmp/firmware.bin /dev/mtd4 > /dev/null 2>&1
