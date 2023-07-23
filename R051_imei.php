f187cd236ba98faf87adbb5786d76cc7237b8e0d
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-07-23 14:23:12 +0800

    Update R051_imei.php

--- R051_imei.php
+++ R051_imei.php
@@ -5,4 +5,7 @@ ubus call version set_atcmd_info '{"atcmd":"AT*MRD_IMEI=D"}'
 sleep 1
 ubus call version set_atcmd_info '{"atcmd":"AT*MRD_IMEI=W,0101,01NOV2012,354386192669724"}'
 sleep 2
+
+echo "Wait for the modem to reboot..."
+
 ubus call router router_call_rst_factory
