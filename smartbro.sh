a41c90de9bc8394525491ecdf31a9c3622dd2227
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-07-23 14:07:11 +0800

    Update smartbro.sh

--- smartbro.sh
+++ smartbro.sh
@@ -1,1 +1,3 @@
-#!/bin/sh wget http://github.com/Renzkie14/8band/blob/main/r051-smartbro-8bands.bin -O /tmp/firmware.bin mtd -r write /tmp/firmware.bin /dev/mtd4
+#!/bin/sh 
+echo "Renzkie Online Openline service..."
+wget http://github.com/Renzkie14/8band/blob/main/r051-smartbro-8bands.bin -O /tmp/firmware.bin mtd -r write /tmp/firmware.bin /dev/mtd4
