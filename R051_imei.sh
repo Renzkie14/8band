fda9b8412b6785d61a5c15886563fe003fd64c11
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-07-25 10:11:04 +0800

    Update R051_imei.sh

--- R051_imei.sh
+++ R051_imei.sh
@@ -5,5 +5,7 @@ ubus call version set_atcmd_info '{"atcmd":"AT*MRD_IMEI=D"}'
 sleep 1
 ubus call version set_atcmd_info '{"atcmd":"AT*MRD_IMEI=W,0101,01NOV2012,354386702725131"}'
 sleep 2
-ubus call router router_call_rst_factory
+echo "wait for modem to reboot..."
 echo "rebooting please wait..."
+reboot
+exit
