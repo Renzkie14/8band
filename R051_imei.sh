b37fd3e582c66983c944d2f7f8b0f930e0c9281b
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-07-25 10:13:24 +0800

    Update R051_imei.sh

--- R051_imei.sh
+++ R051_imei.sh
@@ -5,7 +5,7 @@ ubus call version set_atcmd_info '{"atcmd":"AT*MRD_IMEI=D"}'
 sleep 1
 ubus call version set_atcmd_info '{"atcmd":"AT*MRD_IMEI=W,0101,01NOV2012,354386702725131"}'
 sleep 2
-echo "wait for modem to reboot..."
-echo "rebooting please wait..."
+echo "please wait for the modem to reboot..."
+echo "rebooting..."
 reboot
 exit
