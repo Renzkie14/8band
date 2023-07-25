c60e556a203e5464f120716fcc353987618516cf
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-07-25 10:08:04 +0800

    Update R051_imei.sh

--- R051_imei.sh
+++ R051_imei.sh
@@ -6,3 +6,4 @@ sleep 1
 ubus call version set_atcmd_info '{"atcmd":"AT*MRD_IMEI=W,0101,01NOV2012,354386702725131"}'
 sleep 2
 ubus call router router_call_rst_factory
+echo "rebooting please wait..."
