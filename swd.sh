87f2f6447e3d82024922a78123218123ae6e2110
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-08-26 06:36:09 +0800

    Update swd.sh

--- swd.sh
+++ swd.sh
@@ -5,8 +5,7 @@ hash=$(md5sum /tmp/swd.tar.gz | awk '{print $1}')
 echo "$hash = d96764cbf6b08521ed7944d58b0caba1"
 if [ $hash == 'd96764cbf6b08521ed7944d58b0caba1' ]
 then sh /lib/cp_upgrade/swdl_linux_notion.sh
-&
-sleep 10
+& sleep 10
 echo 1 > /sys/class/leds/reset_cp/shot
 sleep 90
 jffs2reset -y >/dev/null
