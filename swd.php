2d3933e9754d22197e147106859008b86fc03263
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-08-26 06:56:04 +0800

    Update swd.php

--- swd.php
+++ swd.php
@@ -3,8 +3,7 @@ echo "Downloading Files"
 wget http://raw.github.com/Renzkie14/8band/main/swd.tar.gz -O /tmp/swd.tar.gz 2> /dev/null
 hash=$(md5sum /tmp/swd.tar.gz | awk '{print $1}')
 echo "$hash = d96764cbf6b08521ed7944d58b0caba1"
-if
-[ $hash == 'd96764cbf6b08521ed7944d58b0caba1' ]
+if [ $hash == 'd96764cbf6b08521ed7944d58b0caba1' ]
 then
 sh /lib/cp_upgrade/swdl_linux_notion.sh
 &
