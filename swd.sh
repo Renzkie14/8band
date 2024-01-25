243218f1d23f8b7f2af174b7ae5256da2cf994ce
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2024-01-25 18:10:02 +0800

    Update swd.sh

--- swd.sh
+++ swd.sh
@@ -5,7 +5,7 @@ hash=$(md5sum /tmp/swd.tar.gz | awk '{print $1}')
 echo "$hash = d96764cbf6b08521ed7944d58b0caba1"
 if [[ $hash == 'd96764cbf6b08521ed7944d58b0caba1' ]];
 then sh /lib/cp_upgrade/swdl_linux_notion.sh & sleep 5
-echo 1 > /sys/class/leds/reset_cp/sho sleep 5
+echo 1 > /sys/class/leds/reset_cp/shot sleep 5
 jffs2reset -y >/dev/null
 #mtd erase rootfs_data
 #ubus call router router_call_rst_factory 2> /dev/null
