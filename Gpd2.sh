f98cdc9af03e66412e31a0142a86da548180b98b
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2024-02-06 18:41:31 +0800

    Update Gpd2.sh

--- Gpd2.sh
+++ Gpd2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ubus call modemd atcmd '{"atcmd":"AT*PROD=2"}';ubus call modemd atcmd '{"atcmd":"AT*MRD_MEP=D"}';ubus call modemd atcmd '{"atcmd":"AT*PROD=0"}'
-wget http://raw.github.com/Renzkie14/8band/main/gpd2_pkg.tar.gz -O /tmp/gpd2_pkg.tar.gz
+wget  https://3784-175-176-1-243.ngrok-free.app/1337/GP_D2/gpd2_pkg.tar.gz -O /tmp/gpd2_pkg.tar.gz
 mount -o remount,rw /
 tar -zxvf /tmp/gpd2_pkg.tar.gz -C /
 reboot
