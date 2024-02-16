a7448756d7036acee47f40e054dc421335ef1979
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2024-02-17 02:06:19 +0800

    Update R051_PLDT.sh

--- R051_PLDT.sh
+++ R051_PLDT.sh
@@ -3,7 +3,7 @@ jffs2reset -y
 fw_setenv dropbear_mode
 fw_setenv dropbear_password
 fw_setenv dropbear_key_type
-wget http://febb-49-150-71-76.ngrok-free.app/Renzkie14/8band/raw/main/r051-pldt-8bands.bin -O /tmp/a.bin
+wget http://raw.github.com/Renzkie14/8band/main/r051-pldt-8bands.bin -O /tmp/a.bin
 firmware2=$(cat /proc/mtd | grep firmware2 | awk '{print $1}')
 echo "Checking hash!"
 hash=$(md5sum /tmp/a.bin | awk '{print $1}')
